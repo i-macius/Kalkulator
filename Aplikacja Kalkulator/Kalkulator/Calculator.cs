067d08470a1a9e92ca1ba2fba35b766cfe30208b
Igor <igormaciejewski68@gmail.com>
2023-05-29 17:49:30 +0200

    Adding

---
+++ Aplikacja Kalkulator/Kalkulator/Calculator.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kalkulator
+{
+    public class Calculator
+    {
+        public int Add(int value1, int value2) => value1 + value2;
+    }
+    
+}
